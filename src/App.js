c8b9f83e47a7806c79b3f34f435931943fdbb419
sanjith211 <sanjith211@gmail.com>
2024-05-27 16:40:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggle = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggle}>{isVisible ? "Show" : "Hide"}</button>
+      {!isVisible && <div>Hello, Welcome to react</div>}
     </div>
   );
 }
